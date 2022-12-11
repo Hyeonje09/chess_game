dd032c2e7104868ce1af3b223fcbd138d446711c
hyeonje <jhj37299455@gmail.com>
2022-12-12 02:59:51 +0900

    오브젝트 클릭 이벤트

--- chess_game/소스.cpp
+++ chess_game/소스.cpp
@@ -17,9 +17,9 @@ float	camera_radius;
 
 float	moveX, moveY;
 
-float	moveW = 0.0;
+int cnt;
 
-int g_pawnID, g_kingID, g_bishopID, g_KnightID, g_queenID, g_rookID;
+GLuint g_pawnID, g_kingID, g_bishopID, g_KnightID, g_queenID, g_rookID;
 
 //********************* Material Data ****************//
 typedef struct materialStruct {
@@ -243,20 +243,22 @@ public:
 
 CModel a, b, c, d, e, f;
 
+void light() {
+	glMaterialfv(GL_FRONT, GL_AMBIENT, currentMaterials->ambient);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, currentMaterials->diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, currentMaterials->specular);
+	glMaterialf(GL_FRONT, GL_SHININESS, currentMaterials->shininess);
+}
+
 void draw_chessboard_b() {
 	currentMaterials = &blackPlasticMaterials;
-	glMaterialfv(GL_FRONT, GL_AMBIENT, currentMaterials->ambient);
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, currentMaterials->diffuse);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, currentMaterials->specular);
-	glMaterialf(GL_FRONT, GL_SHININESS, currentMaterials->shininess);
+	light();
 	glutSolidCube(0.5);
 }
+
 void draw_chessboard_w() {
 	currentMaterials = &whitePlasticMaterials;
-	glMaterialfv(GL_FRONT, GL_AMBIENT, currentMaterials->ambient);
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, currentMaterials->diffuse);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, currentMaterials->specular);
-	glMaterialf(GL_FRONT, GL_SHININESS, currentMaterials->shininess);
+	light();
 	glutSolidCube(0.5);
 }
 
@@ -273,7 +275,6 @@ void init(void)
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_LIGHTING);
 
-
 	glEnable(GL_LIGHT0);
 	currentLighting = &coloredLighting;
 	glLightfv(GL_LIGHT0, GL_AMBIENT, currentLighting->ambient);
@@ -282,7 +283,7 @@ void init(void)
 	glLightfv(GL_LIGHT0, GL_POSITION, currentLighting->position);
 	glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, currentLighting->cutoff);
 	glLightf(GL_LIGHT0, GL_SPOT_EXPONENT, currentLighting->exponent);
-
+	
 
 	glEnable(GL_LIGHT1);
 	currentLighting = &whiteLighting;
@@ -292,12 +293,14 @@ void init(void)
 	glLightfv(GL_LIGHT1, GL_POSITION, currentLighting->position);
 }
 
-void changeSize(int w, int h)
-{
+GLfloat ratio;
+
+void changeSize(int w, int h) {
 	//창이 아주 작을 때, 0 으로 나누는 것을 예방
 	if (h == 0)
 		h = 1;
-	float ratio = 1.0 * w / h;
+	
+	ratio = 1.0 * w / h;
 
 	//좌표계를 수정하기 전에 초기화
 	glMatrixMode(GL_PROJECTION);
@@ -319,7 +322,6 @@ void chessman(CModel m) {
 	int nFaces;
 
 	nFaces = m.objs[0].f.size();
-
 
 	for (int k = 0; k < nFaces; k++) {
 		int nPoints = m.objs[0].f[k].v_pairs.size();
@@ -360,7 +362,6 @@ void pawn() {
 
 	glEndList();
 }
-
 void rook() {
 	g_rookID = g_pawnID + 1;
 	glNewList(g_rookID, GL_COMPILE);
@@ -377,7 +378,6 @@ void rook() {
 
 	glEndList();
 }
-
 void bishop() {
 	g_bishopID = g_rookID + 1;
 	glNewList(g_bishopID, GL_COMPILE);
@@ -393,7 +393,6 @@ void bishop() {
 
 	glEndList();
 }
-
 void Knight() {
 	g_KnightID = g_bishopID + 1;
 	glNewList(g_KnightID, GL_COMPILE);
@@ -410,7 +409,6 @@ void Knight() {
 
 	glEndList();
 }
-
 void queen() {
 	g_queenID = g_KnightID + 1;
 	glNewList(g_queenID, GL_COMPILE);
@@ -427,7 +425,6 @@ void queen() {
 
 	glEndList();
 }
-
 void king() {
 	g_kingID = g_queenID + 1;
 	glNewList(g_kingID, GL_COMPILE);
@@ -443,6 +440,126 @@ void king() {
 	chessman(f);
 
 	glEndList();
+}
+
+int px = -2.0, py = -1.0;
+void pawn_move(int x, int y) {
+	glPushMatrix();
+	glTranslatef(0.25, -2.0, -1.0);
+	glRotatef(90.0, 0.0, 0.0, -90.0);
+	glScalef(0.03, 0.03, 0.03);
+	glCallList(g_pawnID);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(0.25, -1.5, -1.0);
+	glRotatef(90.0, 0.0, 0.0, -90.0);
+	glScalef(0.03, 0.03, 0.03);
+	glCallList(g_pawnID);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(0.25, -1.0, -1.0);
+	glRotatef(90.0, 0.0, 0.0, -90.0);
+	glScalef(0.03, 0.03, 0.03);
+	glCallList(g_pawnID);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(0.25, -0.5, -1.0);
+	glRotatef(90.0, 0.0, 0.0, -90.0);
+	glScalef(0.03, 0.03, 0.03);
+	glCallList(g_pawnID);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(0.25, 0.0, -1.0);
+	glRotatef(90.0, 0.0, 0.0, -90.0);
+	glScalef(0.03, 0.03, 0.03);
+	glCallList(g_pawnID);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(0.25, 0.5, -1.0);
+	glRotatef(90.0, 0.0, 0.0, -90.0);
+	glScalef(0.03, 0.03, 0.03);
+	glCallList(g_pawnID);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(0.25, 1.0, -1.0);
+	glRotatef(90.0, 0.0, 0.0, -90.0);
+	glScalef(0.03, 0.03, 0.03);
+	glCallList(g_pawnID);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(0.25, 1.5, -1.0);
+	glRotatef(90.0, 0.0, 0.0, -90.0);
+	glScalef(0.03, 0.03, 0.03);
+	glCallList(g_pawnID);
+	glPopMatrix();
+}
+void knight_move() {
+	glPushMatrix();
+	glTranslatef(0.25, -1.5, -1.5);
+	glRotatef(90.0, 0.0, 0.0, -90.0);
+	glScalef(0.03, 0.03, 0.03);
+	glCallList(g_KnightID);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(0.25, 1.0, -1.5);
+	glRotatef(90.0, 0.0, 0.0, -90.0);
+	glScalef(0.03, 0.03, 0.03);
+	glCallList(g_KnightID);
+	glPopMatrix();
+}
+void rook_move() {
+	glPushMatrix();
+	glTranslatef(0.25, -2.0, -1.5);
+	glRotatef(90.0, 0.0, 0.0, -90.0);
+	glScalef(0.03, 0.03, 0.03);
+	glCallList(g_rookID);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(0.25, 1.5, -1.5);
+	glRotatef(90.0, 0.0, 0.0, -90.0);
+	glScalef(0.03, 0.03, 0.03);
+	glCallList(g_rookID);
+	glPopMatrix();
+}
+void bishop_move() {
+	glPushMatrix();
+	glTranslatef(0.25, -1.0, -1.5);
+	glRotatef(90.0, 0.0, 0.0, -90.0);
+	glScalef(0.03, 0.03, 0.03);
+	glCallList(g_bishopID);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(0.25, 0.5, -1.5);
+	glRotatef(90.0, 0.0, 0.0, -90.0);
+	glScalef(0.03, 0.03, 0.03);
+	glCallList(g_bishopID);
+	glPopMatrix();
+}
+void queen_move() {
+	glPushMatrix();
+	glTranslatef(0.25, 0.0, -1.5);
+	glRotatef(90.0, 0.0, 0.0, -90.0);
+	glScalef(0.03, 0.03, 0.03);
+	glCallList(g_queenID);
+	glPopMatrix();
+}
+void king_move() {
+	glPushMatrix();
+	glTranslatef(0.25, -0.5, -1.5);
+	glRotatef(90.0, 0.0, 0.0, -90.0);
+	glScalef(0.03, 0.03, 0.03);
+	glCallList(g_kingID);
+	glPopMatrix();
 }
 
 void display(void) {
@@ -489,50 +606,81 @@ void display(void) {
 		}
 	}
 
-	glPushMatrix();
-	glTranslatef(0.25, -2.0, moveW);
-	glRotatef(90.0, 0.0, 0.0, -90.0);
-	glScalef(0.03, 0.03, 0.03);
-	glCallList(g_pawnID);
-	glPopMatrix();
+	
+	glPushName(g_pawnID);
+	pawn_move(px, py);
+	
+	glPushName(g_rookID);
+	rook_move();
+	
+	glPushName(g_bishopID);
+	bishop_move();
+	
+	glPushName(g_KnightID);
+	knight_move();
+	
+	glPushName(g_queenID);
+	queen_move();
 
-	glPushMatrix();
-	glTranslatef(0.25, -1.5, moveW);
-	glRotatef(90.0, 0.0, 0.0, -90.0);
-	glScalef(0.03, 0.03, 0.03);
-	glCallList(g_kingID);
-	glPopMatrix();
-
-	glPushMatrix();
-	glTranslatef(0.25, -1.0, moveW);
-	glRotatef(90.0, 0.0, 0.0, -90.0);
-	glScalef(0.03, 0.03, 0.03);
-	glCallList(g_bishopID);
-	glPopMatrix();
-
-	glPushMatrix();
-	glTranslatef(0.25, -0.5, moveW);
-	glRotatef(90.0, 0.0, 0.0, -90.0);
-	glScalef(0.03, 0.03, 0.03);
-	glCallList(g_KnightID);
-	glPopMatrix();
-
-	glPushMatrix();
-	glTranslatef(0.25, 0.0, moveW);
-	glRotatef(90.0, 0.0, 0.0, -90.0);
-	glScalef(0.03, 0.03, 0.03);
-	glCallList(g_queenID);
-	glPopMatrix();
-
-	glPushMatrix();
-	glTranslatef(0.25, 0.5, moveW);
-	glRotatef(90.0, 0.0, 0.0, -90.0);
-	glScalef(0.03, 0.03, 0.03);
-	glCallList(g_rookID);
-	glPopMatrix();
+	glPushName(g_kingID);
+	king_move();
 
 	glFlush();
 	glutSwapBuffers();
+}
+
+void ProcessSelect(GLuint index[64], GLuint hits, int x, int y) {
+	if (hits == 2) {
+		if (index[3] == g_pawnID) {
+			cout << "pawn" << endl;
+		}
+		else if (index[3] == g_kingID) {
+			cout << "king" << endl;
+		}
+		else if (index[3] == g_KnightID) {
+			cout << "knight" << endl;
+		}
+		else if (index[3] == g_queenID) {
+			cout << "queen" << endl;
+		}
+		else if (index[3] == g_bishopID) {
+			cout << "bishop" << endl;
+		}
+		else if (index[3] == g_rookID) {
+			cout << "rook" << endl;
+		}
+	}
+	else {
+		cout << "이동할 좌표 : " << x << ", " << y << endl;
+		pawn_move(x, y);
+	}
+}
+
+void SelectObject(int x, int y) {
+	GLuint selectBuff[64];
+	GLint hits, viewport[4];
+	
+	glSelectBuffer(64, selectBuff);
+	glGetIntegerv(GL_VIEWPORT, viewport);
+
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glRenderMode(GL_SELECT);
+	glLoadIdentity();
+	gluPickMatrix(x, viewport[3] - y, 2, 2, viewport);
+	gluPerspective(45.0f, ratio, 0.1f, 100.0f);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	display();
+	hits = glRenderMode(GL_RENDER);
+	cout << "hits : " << hits << endl;
+	if (hits > 0) {
+		cnt++;
+		ProcessSelect(selectBuff, hits, x, y);
+	}
+	glMatrixMode(GL_PROJECTION);
+	glPopMatrix();
+	glMatrixMode(GL_MODELVIEW);
 }
 
 void SpecialKey(int key, int x, int y) {
@@ -558,28 +706,16 @@ void SpecialKey(int key, int x, int y) {
 
 //button : 왼쪽 or 오른쪽 버튼 / state : 눌렸는지 떼었는지 / x, y : 위치 정보(픽셀 단위)
 void mouse(int button, int state, int x, int y) {
-	int cnt = 0;
-	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && cnt == 0) {
-		// 왼쪽 버튼을 누르면 어떤 일을 수행해야 하는가?
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		SelectObject(x, y);
+		cout << ">> cnt : " << cnt << endl;
+	}
+
+	/*
+	else if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
 		
 	}
-
-	if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
-		
-	}
-	
-	glutPostRedisplay();
-}
-
-void MyKey(unsigned char key, int x, int y) {
-	switch (key) {
-	case 'w':	moveW += 0.5;
-		break;
-	case 's': moveW -= 0.5;
-		break;
-
-	default: break;
-	}
+	*/
 	
 	glutPostRedisplay();
 }
@@ -594,22 +730,14 @@ int main(int argc, char** argv) {
 	init();
 
 	pawn();
-	cout << g_pawnID << endl;
 	rook();
-	cout << g_rookID << endl;
 	bishop();
-	cout << g_bishopID << endl;
 	Knight();
-	cout << g_KnightID << endl;
 	queen();
-	cout << g_queenID << endl;
 	king();
-	cout << g_kingID << endl;
-
 
 	glutDisplayFunc(display);
 	glutSpecialFunc(SpecialKey);
-	glutKeyboardFunc(MyKey);
 	glutMouseFunc(mouse);
 	glutReshapeFunc(changeSize);
 	glutIdleFunc(display);
